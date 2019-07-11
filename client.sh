05d726afc2cb24380b074e241c4a5365a7a5312d
miyata-hiroo <hhrrrrr0329@gmail.com>
2019-07-11 13:14:24 +0900

    change client.sh

--- client.sh
+++ client.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
+if [ "$1" == "miyata" ] ; then
+    IP="157.82.201.83"
+elif [ "$1" == "shimono" ] ; then
+    IP="157.82.202.64"
+elif [ "$1" == "myself" ] ; then
+    IP="127.0.0.1"
+fi
+
 gcc -g -o i3_fft i3.c fft.c -Wall -lm
-./i3_fft $1 50000
+./i3_fft $IP 50000
